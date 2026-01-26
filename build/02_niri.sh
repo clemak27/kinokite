a868d9cb65aec1fcb66a4d5508408742658d05d4
clemak27 <clemak27@mailbox.org>
2026-01-26 15:42:29 +0100

    feat: add dank fedora build
    feat: add dank fedora build

---
+++ build/02_niri.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+dnf -y copr enable avengemedia/dms
+dnf -y copr enable scottames/ghostty
+
+dnf -y install \
+  brightnessctl \
+  cava \
+  chezmoi \
+  ddcutil \
+  dgop \
+  dms \
+  dms-cli \
+  dms-greeter \
+  dsearch \
+  fpaste \
+  ghostty \
+  glycin-thumbnailer \
+  gnome-disk-utility \
+  gnome-keyring \
+  gnome-keyring-pam \
+  greetd \
+  greetd-selinux \
+  matugen \
+  nautilus \
+  nautilus-python \
+  niri \
+  openrgb-udev-rules \
+  openssh-askpass \
+  pipewire \
+  playerctl \
+  quickshell \
+  steam-devices \
+  udiskie \
+  webp-pixbuf-loader \
+  wireplumber \
+  wl-clipboard \
+  xdg-desktop-portal-gnome \
+  xdg-desktop-portal-gtk \
+  xdg-terminal-exec \
+  xdg-user-dirs \
+  xwayland-satellite
+
+# fcitx5-mozc \
+
+dnf -y remove \
+  alacritty
+
+dnf install -y --setopt=install_weak_deps=False \
+  kf6-kirigami \
+  qt6ct \
+  plasma-breeze \
+  breeze-cursor-theme \
+  kf6-qqc2-desktop-style
+
+dnf install -y \
+  default-fonts-core-emoji \
+  google-noto-color-emoji-fonts \
+  google-noto-emoji-fonts \
+  glibc-all-langpacks \
+  default-fonts
+
+systemctl enable --global dms.service
+systemctl enable --global gnome-keyring-daemon.service
+systemctl enable --global gnome-keyring-daemon.socket
+
+systemctl enable greetd
+systemctl enable firewalld
+systemctl enable brew-setup.service
+
+# systemctl enable --global fcitx5.service
+# systemctl enable --global udiskie.service
+# systemctl preset --global udiskie
+
+# improve fingerprint auth
+install -Dpm0644 -t /usr/lib/pam.d/ /usr/share/quickshell/dms/assets/pam/*
+sed --sandbox -i -e '/gnome_keyring.so/ s/-auth/auth/ ; /gnome_keyring.so/ s/-session/session/' /etc/pam.d/greetd
+
+# user for greeter
+tee /usr/lib/sysusers.d/greeter.conf << 'EOF'
+g greeter 767
+u greeter 767 "Greetd greeter"
+EOF
+
+sed -i 's;^NAME=.*;NAME=\"dank fedora\";g' /usr/lib/os-release
+sed -i 's;^PRETTY_NAME=.*;NAME=\"dank fedora 43\";g' /usr/lib/os-release
+sed -i 's;^NAME=.*;NAME=\"dank fedora\";g' /usr/lib/os-release
